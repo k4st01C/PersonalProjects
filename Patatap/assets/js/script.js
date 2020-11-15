902ef91604028eb1a3ae528be8c7ea47978cff74
Kemal Aydık <kemalaydik@gmail.com>
2020-11-16 02:35:36 +0300

    -

--- Patatap/assets/js/script.js
+++ Patatap/assets/js/script.js
@@ -1,7 +1,23 @@
-var path = new Path();
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [100, -50]);
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function rnd(x) {
+  return Math.floor(Math.random() * x);
+}
+
+var radius = rnd(90) + 10;
+
+function rndParameters() {
+  var x = window.innerWidth - radius * 2;
+  var y = window.innerHeight - radius * 2;
+  return new Point(rnd(x)+radius, rnd(y)+radius);
+}
+
+var myCircle = new Path.Circle(rndParameters(), radius);
+myCircle.fillColor = getRandomColor();
