6adf68af2d40b279834aee90da58efae22e877e1
Kemal Aydık <53663439+k4st01C@users.noreply.github.com>
2020-11-24 01:46:37 +0300

    -

--- Patatap/assets/js/script.js
+++ Patatap/assets/js/script.js
@@ -11,13 +11,37 @@ function rnd(x) {
   return Math.floor(Math.random() * x);
 }
 
-var radius = rnd(90) + 10;
+var radius = rnd(10) + 90;
 
 function rndParameters() {
   var x = window.innerWidth - radius * 2;
   var y = window.innerHeight - radius * 2;
-  return new Point(rnd(x)+radius, rnd(y)+radius);
+  return new Point(rnd(x) + radius, rnd(y) + radius);
 }
 
-var myCircle = new Path.Circle(rndParameters(), radius);
-myCircle.fillColor = getRandomColor();
+var circles = [];
+
+var text = new PointText({
+  point: view.center,
+  content: 'Click here to focus and then press some keys.',
+  justification: 'center',
+  fontSize: 15,
+});
+
+function onKeyDown(event) {
+  var myCircle = new Path.Circle(rndParameters(), radius);
+  myCircle.fillColor = getRandomColor();
+  circles.push(myCircle);
+
+  sound.play();
+}
+
+function onFrame() {
+  for (var i = 0; i < circles.length; i++) {
+    circles[i].scale(0.93);
+  }
+}
+
+var sound = new Howl({
+  src: ['assets/sounds/clay.mp3'],
+});
