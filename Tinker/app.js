ce72c12bd2266a7d176bda759cc28f15801f8509
Kemal Aydık <53663439+k4st01C@users.noreply.github.com>
2020-12-22 12:47:22 +0200

    -

--- Tinker/app.js
+++ Tinker/app.js
@@ -1,28 +1,26 @@
 const axios = require('axios');
 const express = require('express');
 const app = express();
-
-
-axios
-  .get('http://www.omdbapi.com/', {
-    params: {
-      apikey: '8e9a10f',
-      s: 'tes',
-      plot: 'full',
-    },
-  })
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send;
+  axios
+    .get('http://www.omdbapi.com/', {
+      params: {
+        apikey: '8e9a10f',
+        s: 'tes',
+      },
+    })
+    .then(function (response) {
+      res.render('results',{results:response.data.Search});
+      console.log(response.data.Search);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
 });
 
 app.listen('3000', () => {
