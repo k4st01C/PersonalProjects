0d867bb4a1cc7b918b6a407c6b2e1f2ce504509a
Kemal Aydık <kemalaydik@gmail.com>
2021-01-12 10:40:30 +0300

    -

--- restfulBlogs/app.js
+++ restfulBlogs/app.js
@@ -3,35 +3,91 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   app = express();
 
-app.set('view engine',ejs);
+app.set('view engine', ejs);
 app.use(express.static('public'));
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/restfulBlogs');
 
-const blogSchema=new mongoose.Schema({
-    name:String,
-    img:String,
-    article:String,
-    
+const blogSchema = new mongoose.Schema({
+  name: String,
+  img: String,
+  article: String,
 });
 
-mongoose.model('Blog',blogSchema);
-
-
-
-
+const Blog = mongoose.model('Blog', blogSchema);
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
+
 app.get('/blogs', (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    if (err) console.log(err);
+    else {
+      res.render('index', { blogs });
+    }
+  });
   res.render('home');
 });
 
+app.get('/blogs/new', (req, res) => {
+  res.send('new');
+});
+
+app.post('/blogs', (req, res) => {
+  const name = req.body.name,
+    img = req.body.img,
+    article = req.body.article;
+
+  Blog.create({ name, img, desc }, (err, blog) => {
+    if (err) console.log(err);
+    else res.redirect('/blogs');
+  });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  Blog.findById(req.params.id, (err, blog) => {
+    if (err) console.log(err);
+    else {
+      res.render('show', { blog });
+    }
+  });
+});
+
+app.get('/blogs/:id/edit',(req,res)=>{
+    Blog.findById(req.params.id, (err, blog) => {
+        if (err) console.log(err);
+        else {
+          res.render('edit', { blog });
+        }
+      });
+});
+
+app.put('/blogs/:id',(req,res)=>{
+    const name = req.body.name,
+    img = req.body.img,
+    article = req.body.article;
+
+    Blog.findByIdAndUpdate (req.params.id,{name,img,article}, (err, blog) => {
+        if (err) console.log(err);
+        else {
+          res.redirect('/blogs')
+        }
+      });
+})
+
+app.delete('/blogs/:id',(req,res)=>{
+    Blog.findByIdAndDelete (req.params.id, (err, blog) => {
+        if (err) console.log(err);
+        else {
+          res.redirect('/blogs')
+        }
+      });
 
 
+});
 
 app.listen('3000', () => {
   console.log('starting');
